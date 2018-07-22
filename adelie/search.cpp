2487e923c2f9c0fe4e01be6714ef06acd1d5630c
burokoron <39452528+burokoron@users.noreply.github.com>
2018-07-22 22:33:54 +0900

    Add history and activate futility cut

--- adelie/search.cpp
+++ adelie/search.cpp
@@ -15,6 +15,9 @@ int killer[] = { 7, 10, 25, 28,
 				14, 15, 20, 21,
 				 2,  3, 12, 17, 18, 23, 32, 33,
 				 0,  5, 30, 35 };
+
+HISTORY history1[288], history2[288];
+
 // xorshift
 // 初期化
 void InitXor() {
@@ -28,12 +31,57 @@ unsigned long Xor128() {
 	t = (x ^ (x << 11));x = y;y = z;z = w; return(w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
 }
 
+// ランダムムーブ
+void RandomMove(BOARD *board, uint64_t *best_move) {
+	do {
+		best_move[0] = (uint64_t)1 << Xor128() % 36;
+		best_move[1] = Xor128() % 8;
+	} while ((board->black & best_move[0]) != 0 || (board->white & best_move[0]) != 0);
+}
+
+// history
+// 初期化
+void Init_history() {
+	for (int i = 0;i < 288;i++) {
+		history1[i].num = 0;
+	}
+}
+
+// 最善手になった回数の保存
+void Save_history(const uint64_t *best_move) {
+	uint64_t pos = 1;
+	int i;
+	for (i = 0;i<36;i++) {
+		if ((best_move[0] & pos) != 0) break;
+		pos <<= 1;
+	}
+	history1[i * 8 + int(best_move[1])].move[0] = best_move[0];
+	history1[i * 8 + int(best_move[1])].move[1] = best_move[1];
+	history1[i * 8 + int(best_move[1])].num++;
+}
+
+// qソート比較関数
+int cmp(const void *p, const void *q) {
+	return ((HISTORY*)q)->num - ((HISTORY*)p)->num;
+}
+
+// 出現回数が多い順にソート
+void Sort_history() {
+	for (int i = 0;i < 288; i++) {
+		history2[i].move[0] = history1[i].move[0];
+		history2[i].move[1] = history1[i].move[1];
+		history2[i].num = history1[i].num;
+	}
+
+	qsort(history2, 288, sizeof(HISTORY), cmp);
+}
+
 // 単純alpha-beta(枝刈あり)
 double NegaAlpha(BOARD *board, uint64_t *best_move, CLUSTER *cluster, double alpha, double beta, int deep, int depth) {
 	int winner_flag, entry_num, save_cluster[2] = { -1, 100 };
-	int rand_margin = 5;
+	int rand_margin = 5, futility_margin;
 	double var, max = -MY_INFINITY;
-	uint64_t move_pos[2], best_pos[2] = { (uint64_t)100, (uint64_t)100 }, save_move[2];
+	uint64_t move_pos[2], best_pos[2] = { (uint64_t)100, (uint64_t)100 }, save_move[2] = {100, 100};
 
 	// ゲーム終了なら値を返す
 	winner_flag = EndJudge(board);
@@ -46,12 +94,6 @@ double NegaAlpha(BOARD *board, uint64_t *best_move, CLUSTER *cluster, double alp
 		if (board->turn == BLACK) return -MY_INFINITY + deep;
 		else return MY_INFINITY - deep;
 	}
-
-	// futility cut
-	/*if (futility_margin = depth - deep <= 1) {
-		var = Evaluate(board);
-		if (var < alpha - futility_margin * 10 || beta+ futility_margin * 10 < var) return var;
-	}*/
 
 	// 置換表のチェック
 	entry_num = board->key&(entry_count - 1);
@@ -86,6 +128,12 @@ double NegaAlpha(BOARD *board, uint64_t *best_move, CLUSTER *cluster, double alp
 
 	// 局面の評価値を返す
 	if (deep >= depth) return Evaluate(board) + ((float)(Xor128() % (101 * rand_margin)) - (51 * rand_margin)) / 100.0;
+	
+	// futility cut
+	if ((futility_margin = depth - deep) <= 1) {
+		var = Evaluate(board);
+		if (var < alpha - futility_margin * (37 - board->turn) * 2.5 || beta + futility_margin * (37 - board->turn) * 2.5 < var) return var;
+	}
 
 	// 次の1手を読む
 	// 置換表に登録されていた手を先に読む
@@ -105,6 +153,32 @@ double NegaAlpha(BOARD *board, uint64_t *best_move, CLUSTER *cluster, double alp
 		}
 		if (var > max) max = var;
 	}
+
+	// 過去の探索で多く最善手となった手を優先して読む
+	for (int i = 0;i < 288 && alpha < beta && history2[i].num != 0;i++) {
+		if ((board->black & history2[i].move[0]) == 0 && (board->white & history2[i].move[0]) == 0) {
+			DoMoveHash(board, history2[i].move);
+			node++;
+			if (i == 0) var = -NegaAlpha(board, history2[i].move, &cluster[0], -beta, -alpha, deep + 1, depth);
+			else {
+				// null window search
+				var = -NegaAlpha(board, history2[i].move, &cluster[0], -alpha - 0.01, -alpha, deep + 1, depth);
+				if (alpha < var&&var < beta) var = -NegaAlpha(board, history2[i].move, &cluster[0], -beta, -var, deep + 1, depth);
+			}
+			UnDoMoveHash(board, history2[i].move);
+			if (var > alpha) {
+				alpha = var;
+				save_move[0] = history2[i].move[0];
+				save_move[1] = history2[i].move[1];
+				if (deep == 0) {
+					best_move[0] = history2[i].move[0];
+					best_move[1] = history2[i].move[1];
+				}
+			}
+			if (var > max) max = var;
+		}
+	}
+
 	// 全合法手を読む
 	for (int i = 0;i < 36 && alpha < beta;i++) {
 		uint64_t pos = (uint64_t)1 << killer[i];
@@ -135,6 +209,9 @@ double NegaAlpha(BOARD *board, uint64_t *best_move, CLUSTER *cluster, double alp
 			}
 		}
 	}
+
+	// 最善手になった回数を数える
+	if(save_move[1] != 100) Save_history(save_move);
 
 	// 置換表に保存
 	for (int i = 0;i < 3;i++) {
--- adelie/search.h
+++ adelie/search.h
@@ -10,12 +10,34 @@
 #define max(a, b) ((a)>(b)?(a):(b))
 #define min(a, b) ((a)<(b)?(a):(b))
 
+// Å‘Pè‚É‚È‚Á‚½‰ñ”‚ğ‹L˜^
+typedef struct HISTORY {
+	int num;
+	uint64_t move[2];
+}HISTORY;
+
 // xorshift
 // ‰Šú‰»
 void InitXor();
 
 // —”
 unsigned long Xor128();
+
+// history
+// ‰Šú‰»
+void Init_history();
+
+// Å‘Pè‚É‚È‚Á‚½‰ñ”‚Ì•Û‘¶
+void Save_history(const uint64_t *best_move);
+
+// qƒ\[ƒg”äŠrŠÖ”
+int cmp(const void *p, const void *q);
+
+// oŒ»‰ñ”‚ª‘½‚¢‡‚Éƒ\[ƒg
+void Sort_history();
+
+// ƒ‰ƒ“ƒ_ƒ€ƒ€[ƒu
+void RandomMove(BOARD *board, uint64_t *best_move);
 
 // ’Pƒalpha-beta(}Š ‚È‚µ)
 double NegaAlpha(BOARD *board, uint64_t *move_pos, CLUSTER *cluster, double alpha, double beta, int deep, int depth);
